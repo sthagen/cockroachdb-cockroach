cockroachdb/cockroach

kvserver: force loose-coupled truncations with separate engine
When we have separated engines, the apply path forces
loosely-coupled truncation regardless of the
kv.raft_log.loosely_coupled_truncation.enabled cluster setting because
loosely-coupled truncations are mandatory in separate engines world.

Release note: None

Co-Authored-By: roachdev-claude <roachdev-claude-bot@cockroachlabs.com>

pkg/kv/kvserver/replica_app_batch_test.go
@@ -23,20 +23,26 @@ func TestShouldUseLooselyCoupledTruncation(t *testing.T) {
 	st := cluster.MakeTestingClusterSettings()
 	tests := []struct {
 		settingEnabled         bool
+		enginesSeparated       bool
 		raftExpectedFirstIndex kvpb.RaftIndex
 		wantLoose              bool
 	}{
-		{settingEnabled: false, raftExpectedFirstIndex: 0, wantLoose: false},
-		{settingEnabled: false, raftExpectedFirstIndex: 5, wantLoose: false},
-		{settingEnabled: true, raftExpectedFirstIndex: 0, wantLoose: false},
-		{settingEnabled: true, raftExpectedFirstIndex: 5, wantLoose: true},
+		{settingEnabled: false, enginesSeparated: false, raftExpectedFirstIndex: 0, wantLoose: false},
+		{settingEnabled: false, enginesSeparated: false, raftExpectedFirstIndex: 5, wantLoose: false},
+		{settingEnabled: false, enginesSeparated: true, raftExpectedFirstIndex: 0, wantLoose: true},
+		{settingEnabled: false, enginesSeparated: true, raftExpectedFirstIndex: 5, wantLoose: true},
+		{settingEnabled: true, enginesSeparated: false, raftExpectedFirstIndex: 0, wantLoose: false},
+		{settingEnabled: true, enginesSeparated: false, raftExpectedFirstIndex: 5, wantLoose: true},
+		{settingEnabled: true, enginesSeparated: true, raftExpectedFirstIndex: 0, wantLoose: true},
+		{settingEnabled: true, enginesSeparated: true, raftExpectedFirstIndex: 5, wantLoose: true},
 	}
 
 	for _, tc := range tests {
 		t.Run("", func(t *testing.T) {
 			looselyCoupledTruncationEnabled.Override(ctx, &st.SV, tc.settingEnabled)
-			got := shouldUseLooselyCoupledTruncation(&st.SV, tc.raftExpectedFirstIndex)
-			require.Equal(t, tc.wantLoose, got)
+			require.Equal(t, tc.wantLoose, shouldUseLooselyCoupledTruncation(
+				&st.SV, tc.raftExpectedFirstIndex, tc.enginesSeparated,
+			))
 		})
 	}
 }

sthagen/cockroachdb-cockroach

pkg/kv/kvserver/replica_app_batch_test.go
@@ -16,16 +16,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestShouldUseLooselyCoupledTruncation(t *testing.T) {
+func TestUseLooselyCoupledTruncation(t *testing.T) {
 	defer leaktest.AfterTest(t)()
 	defer log.Scope(t).Close(t)
 	ctx := context.Background()
 	st := cluster.MakeTestingClusterSettings()
 	tests := []struct {
+		enginesSeparated       bool
 		settingEnabled         bool
 		raftExpectedFirstIndex kvpb.RaftIndex
 		wantLoose              bool
 	}{
+		// Separated engines use loosely coupled truncations unconditionally.
+		{enginesSeparated: true, settingEnabled: false, raftExpectedFirstIndex: 0, wantLoose: true},
+		{enginesSeparated: true, settingEnabled: false, raftExpectedFirstIndex: 5, wantLoose: true},
+		{enginesSeparated: true, settingEnabled: true, raftExpectedFirstIndex: 0, wantLoose: true},
+		{enginesSeparated: true, settingEnabled: true, raftExpectedFirstIndex: 5, wantLoose: true},
+		// Other tests assume single engine.
 		{settingEnabled: false, raftExpectedFirstIndex: 0, wantLoose: false},
 		{settingEnabled: false, raftExpectedFirstIndex: 5, wantLoose: false},
 		{settingEnabled: true, raftExpectedFirstIndex: 0, wantLoose: false},
@@ -35,8 +42,9 @@ func TestShouldUseLooselyCoupledTruncation(t *testing.T) {
 	for _, tc := range tests {
 		t.Run("", func(t *testing.T) {
 			looselyCoupledTruncationEnabled.Override(ctx, &st.SV, tc.settingEnabled)
-			got := useLooselyCoupledTruncation(&st.SV, tc.raftExpectedFirstIndex)
-			require.Equal(t, tc.wantLoose, got)
+			require.Equal(t, tc.wantLoose, useLooselyCoupledTruncation(
+				&st.SV, tc.raftExpectedFirstIndex, tc.enginesSeparated,
+			))
 		})
 	}
 }
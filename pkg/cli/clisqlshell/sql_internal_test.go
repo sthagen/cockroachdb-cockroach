cockroachdb/cockroach

clisqlshell: add Context.DisableLocalCmds + Context.DisablePasswordCmd
Embedders that run the shell inside a privileged service process
cannot safely expose the metacommands that touch the local
filesystem or shell out (\!, \|, \i, \ir, \o, and \e), since they
execute with the service's UID. \password is a separate concern —
the command is harmless on the server but types the password into
the shell session.

Add two Context fields:

* DisableLocalCmds gates the five filesystem/shell-out commands.
  The dispatcher rejects \!, \|, \i, \ir, and \o with a clear
  error. The external editor (\e), reachable through the bubbline
  editor's own keybinding rather than the dispatcher, is gated at
  editor-init time via a new externalEditorAllowed() method on the
  sqlShell interface.

* DisablePasswordCmd gates \password independently. An embedder may
  reasonably want the filesystem-touching commands off but \password
  on (or vice versa), so they're separate flags.

The existing restricted-mode guard (introduced for CVE-2025-8714)
is left alone; it's a per-session toggle for a different threat
model and continues to short-circuit the dispatcher above the new
checks.

Part of work to make clisqlshell usable as an embedded library.

Release note: None
Epic: None

pkg/cli/clisqlshell/sql_internal_test.go
@@ -268,3 +268,30 @@ func TestHandleEmbedderInterruptIgnoresIdle(t *testing.T) {
 	interruptCh <- struct{}{}
 	time.Sleep(50 * time.Millisecond)
 }
+
+func TestDisableLocalCmds(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	blocked := []string{`\!`, `\| sort`, `\i file.sql`, `\ir file.sql`, `\o output`}
+	for _, line := range blocked {
+		c := setupTestCliState()
+		c.sqlCtx.DisableLocalCmds = true
+		c.lastInputLine = line
+		c.doHandleCliCmd(cliStateEnum(0), cliStateEnum(1))
+		require.Error(t, c.exitErr, "input %q", line)
+		assert.Contains(t, c.exitErr.Error(), "disabled by embedder", "input %q", line)
+	}
+}
+
+func TestDisablePasswordCmd(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	c := setupTestCliState()
+	c.sqlCtx.DisablePasswordCmd = true
+	c.lastInputLine = `\password`
+	c.doHandleCliCmd(cliStateEnum(0), cliStateEnum(1))
+	require.Error(t, c.exitErr)
+	assert.Contains(t, c.exitErr.Error(), "disabled by embedder")
+}

sthagen/cockroachdb-cockroach

pkg/cli/clisqlshell/sql_internal_test.go
@@ -268,3 +268,30 @@ func TestHandleEmbedderInterruptIgnoresIdle(t *testing.T) {
 	interruptCh <- struct{}{}
 	time.Sleep(50 * time.Millisecond)
 }
+
+func TestDisableLocalCmds(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	blocked := []string{`\!`, `\| sort`, `\i file.sql`, `\ir file.sql`, `\o output`}
+	for _, line := range blocked {
+		c := setupTestCliState()
+		c.sqlCtx.DisableLocalCmds = true
+		c.lastInputLine = line
+		c.doHandleCliCmd(cliStateEnum(0), cliStateEnum(1))
+		require.Errorf(t, c.exitErr, "input %q", line)
+		assert.Containsf(t, c.exitErr.Error(), "disabled by embedder", "input %q", line)
+	}
+}
+
+func TestDisablePasswordCmd(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer log.Scope(t).Close(t)
+
+	c := setupTestCliState()
+	c.sqlCtx.DisablePasswordCmd = true
+	c.lastInputLine = `\password`
+	c.doHandleCliCmd(cliStateEnum(0), cliStateEnum(1))
+	require.Error(t, c.exitErr)
+	assert.Contains(t, c.exitErr.Error(), "disabled by embedder")
+}